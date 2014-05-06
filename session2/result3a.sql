e501bb673e35077e4aa9b6609d2dc38e5c94c970
Dylan Ye <dylan_ye@hotmail.com>
2014-05-06 17:05:44 +0200

    added result 3a and first version of result 3b

---
+++ session2/result3a.sql
@@ -0,0 +1,19 @@
+Godfather, The
+Shawshank Redemption, The
+Schindler's List
+Casablanca
+Citizen Kane
+American Beauty
+Godfather: Part II, The
+Star Wars
+Shichinin no samurai
+One Flew Over the Cuckoo's Nest
+Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb
+Rear Window
+Usual Suspects, The
+Paths of Glory
+M
+Almost Famous
+General, The
+Ladri di biciclette
+Ikiru
